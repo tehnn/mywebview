7d36945650c1c084677e00addaa3bfee28fa4eba
guest01 <guest01@admin>
2014-11-09 18:56:10 +0700

    tehnn

---
+++ src/acom/utehn/mywebview/MainActivity.java
@@ -0,0 +1,74 @@
+package acom.utehn.mywebview;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+	final ActionBarActivity activity = this;
+	WebView webView;
+	String strUrl = "http://hdc.plkhealth.go.th";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		webView = (WebView) findViewById(R.id.webView1);
+		webView.getSettings().setJavaScriptEnabled(true);
+
+		webView.setWebChromeClient(new WebChromeClient() {
+			public void onProgressChanged(WebView view, int progress) {
+				activity.setTitle("Loading...");
+				activity.setProgress(progress * 100);
+
+				if (progress == 100)
+					activity.setTitle(R.string.app_name);
+			}
+		});
+
+		
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				
+			}
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return true;
+			}
+		});
+
+		webView.loadUrl(strUrl);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		int id = item.getItemId();		
+		
+		if(id == R.id.action_reload){
+			webView.loadUrl( "javascript:window.location.reload(true)" );
+			return true;
+			
+		}
+		
+		
+		return super.onOptionsItemSelected(item);
+	}
+}
